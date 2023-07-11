1c74538b6d6be33210e8905a65402302b3a6ace1
dbtrnl <daniel.bittar@alumni.usp.br>
2023-07-11 20:14:35 -0300

    feat: Initial project setup
    REST, gRPC and GraphQL endpoints already functional
    with an order creation route.
    
    REST: localhost:8000/order
    GraphQL: localhost:8080/graphql
    gRPC: localhost:50051

---
+++ cmd/ordersystem/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net"
+	"net/http"
+
+	graphql_handler "github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/dbtrnl/fullcycle.go-expert.clean-arch/configs"
+	"github.com/dbtrnl/fullcycle.go-expert.clean-arch/internal/event/handler"
+	"github.com/dbtrnl/fullcycle.go-expert.clean-arch/internal/infra/graph"
+	"github.com/dbtrnl/fullcycle.go-expert.clean-arch/internal/infra/grpc/pb"
+	"github.com/dbtrnl/fullcycle.go-expert.clean-arch/internal/infra/grpc/service"
+	"github.com/dbtrnl/fullcycle.go-expert.clean-arch/internal/infra/web/webserver"
+	"github.com/dbtrnl/fullcycle.go-expert.clean-arch/pkg/events"
+	"github.com/streadway/amqp"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	// mysql
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := sql.Open(configs.DBDriver, fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", configs.DBUser, configs.DBPassword, configs.DBHost, configs.DBPort, configs.DBName))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	rabbitMQChannel := getRabbitMQChannel()
+
+	eventDispatcher := events.NewEventDispatcher()
+	eventDispatcher.Register("OrderCreated", &handler.OrderCreatedHandler{
+		RabbitMQChannel: rabbitMQChannel,
+	})
+
+	createOrderUseCase := NewCreateOrderUseCase(db, eventDispatcher)
+
+	webserver := webserver.NewWebServer(configs.WebServerPort)
+	webOrderHandler := NewWebOrderHandler(db, eventDispatcher)
+	webserver.AddHandler("/order", webOrderHandler.Create)
+	fmt.Printf("Starting web server on port %s...\n", configs.WebServerPort)
+	go webserver.Start()
+	fmt.Printf("Web server initialized.\n")
+
+	grpcServer := grpc.NewServer()
+	createOrderService := service.NewOrderService(*createOrderUseCase)
+	pb.RegisterOrderServiceServer(grpcServer, createOrderService)
+	reflection.Register(grpcServer)
+
+	fmt.Printf("Starting gRPC server on port %s...\n", configs.GRPCServerPort)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", configs.GRPCServerPort))
+	if err != nil {
+		panic(err)
+	}
+	go grpcServer.Serve(lis)
+	fmt.Printf("gRPC server initialized.\n")
+
+	srv := graphql_handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CreateOrderUseCase: *createOrderUseCase,
+	}}))
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	fmt.Printf("Starting GraphQL server and listening for connections on port %s...\n", configs.GraphQLServerPort)
+	err = http.ListenAndServe(":"+configs.GraphQLServerPort, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getRabbitMQChannel() *amqp.Channel {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		panic(err)
+	}
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+	return ch
+}
